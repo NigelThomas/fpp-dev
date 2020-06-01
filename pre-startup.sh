de7f19cace013d9b3011c697146856a419b4ba95
NigelThomas <nigel.cl.thomas@gmail.com>
2020-06-01 12:40:52 +0100

    get data from mounted volume

--- pre-startup.sh
+++ pre-startup.sh
@@ -11,9 +11,16 @@
 echo ... load the test schema 
 $SQLSTREAM_HOME/bin/sqllineClient --run=test_pipeline.sql
 
-cd /home/sqlstream/fpp-dev/gendata/
-mkdir data
-python generate_data.py -F feature_file.csv -c 1000000 -n -t 6 | split --bytes=50M --filter='gzip > data/$FILE.gz' - data
+# For docker image we assume data has been generated already, or will be generated into a source 
+# directory mounted from the host ${HOST_DATA_SOURCE:=$HOME/fpp-data}
+
+# The test ingest assumes the data is gzipped
+
+# cd /home/sqlstream/fpp-dev/gendata/
+# mkdir data
+# python generate_data.py -F feature_file.csv -c 1000000 -n -t 6 | split --bytes=50M --filter='gzip > data/$FILE.gz' - data
+# command line for Mac - doesn't support inline gzip
+# python generate_data.py -F feature_file.csv -c 1000000 -n -t 6 | split -b 50m - data
 
 
 
